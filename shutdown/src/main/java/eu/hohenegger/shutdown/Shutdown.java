57f2d85e9dbff77747a200a0ab86d01aa169e64b
Max Hohenegger <github.com@hohenegger.eu>
2014-11-16 15:59:17 +0100

    extract dll

--- shutdown/src/main/java/eu/hohenegger/shutdown/Shutdown.java
+++ shutdown/src/main/java/eu/hohenegger/shutdown/Shutdown.java
@@ -1,17 +1,34 @@
 package eu.hohenegger.shutdown;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+
 public class Shutdown {
 	public static void main(String[] args) {
+		try {
+			extractDll(ShutdownDll.NATIVE_LIB_NAME + ShutdownDll.EXTENSION);
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+		
 		ShutdownDll dll = (ShutdownDll) ShutdownDll.INSTANCE;
 		if (!dll.isWinShutdownAllowed()) {
 			System.err.println("shutdown not allowed");
-			System.exit(1);
+			System.exit(2);
 		}
 		boolean shutdownSuccess = dll.winShutdown();
 		if (!shutdownSuccess) {
 			System.err.println("shutdown failed");
-			System.exit(2);
+			System.exit(3);
 		}
 		System.exit(0);
+	}
+	
+	public static void extractDll(String name) throws IOException {
+	    InputStream in = Shutdown.class.getResourceAsStream(name);
+	    Files.copy(in, new File(name).getAbsoluteFile().toPath());
 	}
 }
--- shutdown/src/main/java/eu/hohenegger/shutdown/ShutdownDll.java
+++ shutdown/src/main/java/eu/hohenegger/shutdown/ShutdownDll.java
@@ -5,6 +5,8 @@ import com.sun.jna.Native;
 
 public interface ShutdownDll extends Library {
 	public static final String NATIVE_LIB_NAME = "shutdown";
+	public static final String EXTENSION = ".dll";
+	
 	ShutdownDll INSTANCE = (ShutdownDll) Native.loadLibrary(NATIVE_LIB_NAME, ShutdownDll.class);
 	
 	public boolean winShutdown();
